d3168256c82b57239a0bfafede3a853ac3fe054a
Deepa Ann John <deepa.ann.john1@gmail.com>
2020-08-12 14:26:13 +0530

    fully working nginx reverse proxy

--- app1/routes/index.js
+++ app1/routes/index.js
@@ -5,7 +5,7 @@ var insertData = require('../data/mysql')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   insertData();
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express App1' });
 });
 
 module.exports = router;
